8cd6a4e3639a544b321387dfe99c24b2482c6c32
szl <ahsongzhili@163.com>
2021-11-03 21:49:02 +0800

    top100-5

--- src/p5.java
+++ src/p5.java
@@ -28,10 +28,32 @@ public class p5 {
 //        System.out.println(pos[1]);
         return s.substring(pos[0], pos[1] + 1);
     }
+    public String longestPalindrome1(String s)
+    {
+        int[] pos = new int[2];
+        char[] chars = s.toCharArray();
+        int len = chars.length;
+        boolean[][] dp = new boolean[len][len];
+        for (int i= len - 1; i >= 0; i--) {
+            dp[i][i] = true;
+            for (int j = i + 1; j < len; j++) {
+                if (j - i < 3)
+                    dp[i][j] = chars[i] == chars[j];
+                else
+                    dp[i][j] = dp[i + 1][j - 1] && chars[i] == chars[j];
+                if(dp[i][j] && (j - i) > (pos[1] - pos[0])){
+                    pos[0] = i;
+                    pos[1] = j;
+                }
+            }
+        }
+        return s.substring(pos[0],pos[1] + 1);
+    }
 
 
     public static void main(String[] args) {
         p5 p5 = new p5();
         System.out.println(p5.longestPalindrome("aaaa"));
+        System.out.println(p5.longestPalindrome1("aaaa"));
     }
 }
