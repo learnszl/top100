fd85e88afee3726d5d9c1d1350fb895f269c9196
learnszl <ahsongzhili@163.com>
2021-11-20 21:39:58 +0800

    top100-21

---
+++ src/p21.java
@@ -0,0 +1,23 @@
+/**
+ * @Author szl
+ * @Date 2021/11/20 21:30
+ * @Description
+ */
+public class p21 {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(-1);
+        ListNode p = dummyHead;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                p.next = l1;
+                l1 = l1.next;
+            } else {
+                p.next = l2;
+                l2 = l2.next;
+            }
+            p = p.next;
+        }
+        p.next = l1 == null ? l2 : l1;
+        return dummyHead.next;
+    }
+}
