4adb5080be2fca651ddfb9703c66e4e2a83af033
szl <ahsongzhili@163.com>
2021-11-24 14:50:19 +0800

    top100-10

---
+++ src/p10.java
@@ -0,0 +1,30 @@
+public class p10 {
+    public boolean isMatch(String s, String p) {
+        int m = s.length(), n = p.length();
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+        for (int j = 1; j <= n; j++) {
+            dp[0][j] = p.charAt(j - 1) == '*' && dp[0][j - 2];
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (p.charAt(j - 1) == '.') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p.charAt(j - 1) == '*') {
+                    if (p.charAt(j - 2) == s.charAt(i - 1) || p.charAt(j - 2) == '.') {
+                        dp[i][j] = dp[i][j - 2] || dp[i - 1][j] || dp[i][j - 1];
+                    } else
+                        dp[i][j] = dp[i][j - 2];
+                } else {
+                    dp[i][j] = dp[i - 1][j - 1] && p.charAt(j - 1) == s.charAt(i - 1);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+    public static void main(String[] args) {
+        p10 p10 = new p10();
+        System.out.println(p10.isMatch("aaa", "aaa*"));
+    }
+}
